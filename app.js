300926fcf42af56a68a02804688517f3fa8b7a7f
aurorareamur <aurorareamur@gmail.com>
2013-02-09 23:58:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -56,10 +56,34 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+var print_visits = function(req, res){
+/* Connect to the DB and auth */
+require('mongodb').connect(mongourl, function(err, conn){
+    conn.collection('ips', function(err, coll){
+        coll.find({}, {limit:10, sort:[['_id','desc']]}, function(err, cursor){
+            cursor.toArray(function(err, items){
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                for(i=0; i<items.length;i++){
+                    res.write(JSON.stringify(items[i]) + "\n");
+                }
+                res.end();
+            });
+        });
+    });
+});
+}
+
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  record_visit(req, res)
+http.createServer(app).listen(app.get('port'), function (req, res){
+  params = require('http://hello-express-1.cloudfoundry.com/').parse(req.'http://hello-express-1.cloudfoundry.com/');
+  if(params.pathname === '/history') {
+        print_visits(req, res);
+    }
+    else{
+        record_visit(req, res);
+    }
+}).listen(port, host);
   console.log("Express server listening on port " + app.get('port'));
 });
