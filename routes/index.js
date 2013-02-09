04361172d55bfb7c9598d585effd0cd7e79b747f
aurorareamur <aurorareamur@gmail.com>
2013-02-09 19:45:46 +0700

    Update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
-
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ini Tampilan Express Aurora Reamur :D *bisa edit title tog :(*' });
 };
